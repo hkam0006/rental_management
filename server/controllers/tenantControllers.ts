5c60c3a037eec36e0543152ab385dc0886857ced
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2025-03-26 18:49:14 +1100

    added property routes for tenants

--- server/controllers/tenantControllers.ts
+++ server/controllers/tenantControllers.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client';
+import { wktToGeoJSON } from '@terraformer/wkt';
 
 const prisma = new PrismaClient();
 
@@ -56,5 +57,96 @@ export const updateTenant = async (req: Request, res: Response) : Promise<void> 
     res.json(updatedTenant);
   } catch (err: any) {
     res.status(500).json({ message: ` Error updating tenant: ${err.message}` });
+  }
+}
+
+export const getTenantProperties = async (req: Request, res: Response) : Promise<void> => {
+  try {
+    const {cognitoId} = req.params
+    const properties = await prisma.property.findMany({
+      where: {tenants: {some: {cognitoId}}},
+      include: {
+        location: true
+      }
+    })
+    const residencesWithFormattedLocation = await Promise.all(
+      properties.map(async (property) => {
+        const coordinates : {coordinates: string}[] = await prisma.$queryRaw`
+          SELECT ST_asText(coordinates) as coordinates from "Location" where id = ${property.location.id}
+        `
+        const geoJson: any = wktToGeoJSON(coordinates[0]?.coordinates || "")
+        const longitude = geoJson.coordinates[0]
+        const latitude = geoJson.coordinates[1]
+        return {
+          ...property,
+          location: {
+            ...property.location,
+            coordinates: {
+              longitude,
+              latitude
+            }
+          }
+        }
+      })
+    )
+    res.json(residencesWithFormattedLocation)
+  } catch (err: any) {
+    res.status(500).json({message: `Error fetching tenant residences: ${err.message}`})
+  }
+}
+
+export const addFavoriteProperty = async (req: Request, res: Response) : Promise<void> => {
+  try {
+    const {cognitoId, propertyId} = req.params
+    const tenant = await prisma.tenant.findUnique({
+      where: {cognitoId},
+      include: {favorites: true}
+    })
+
+    const propertyIdNumber = Number(propertyId)
+
+    const existingFavorites = tenant?.favorites || []
+
+    if (!existingFavorites.some((fav) => fav.id === propertyIdNumber)) {
+      const updatedTenant = await prisma.tenant.update({
+        where: {cognitoId},
+        data: {
+          favorites: {
+            connect: {id: propertyIdNumber}
+          }
+        },
+        include: {
+          favorites: true
+        }
+      })
+      res.json(updatedTenant)
+    } else {
+      res.status(409).json({message: "Property already in favorites"})
+    }
+    
+  } catch (err: any) {
+    res.status(500).json({message: `Error adding favorite property: ${err.message}`})
+  }
+}
+
+export const removeFavoriteProperty = async (req: Request, res: Response) : Promise<void> => {
+  try {
+    const {cognitoId, propertyId} = req.params
+    const propertyIdNumber = Number(propertyId)
+
+    const updatedTenant = await prisma.tenant.update({
+      where: {cognitoId},
+      data: {
+        favorites: {
+          disconnect: {id: propertyIdNumber}
+        }
+      },
+      include: {
+        favorites: true
+      }
+    })
+    res.json(updatedTenant)
+  } catch (err: any) {
+    res.status(500).json({message: `Error removing favorite property: ${err.message}`})
   }
 }
--- server/routes/tenantRoutes.ts
+++ server/routes/tenantRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import {getTenant, createTenant, updateTenant} from '../controllers/tenantControllers';
+import {getTenant, createTenant, updateTenant, getTenantProperties, addFavoriteProperty, removeFavoriteProperty} from '../controllers/tenantControllers';
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant)
 router.put("/:cognitoId", updateTenant)
+router.get("/:cognitoId/current-residences", getTenantProperties)
 router.post("/", createTenant)
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty)
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty)
 
 export default router;
